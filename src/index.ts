81a51755d056b06113eb5e63f4c9921b3ccae7d4
Dirk Beukes <dirk_beukes@hotmail.com>
2023-04-06 22:52:37 +0200

    feat: added array shuffling

--- src/index.ts
+++ src/index.ts
@@ -115,10 +115,23 @@ function mapToObject<T>(map: Map<string, T>): Record<string, T> {
   return obj;
 }
 
+function shuffleArray<T>(array: T[]): T[] {
+  const shuffled = [...array];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
+}
+
 const rawEmails = [...TEST_SET_1, ...TEST_SET_2];
 const parsedEmails = rawEmails.map(parseEmail);
-const emailTree = buildEmailTree(parsedEmails);
+const shuffledEmails = shuffleArray(parsedEmails);
+const emailTree = buildEmailTree(shuffledEmails);
 const sortedEmailChains = traverseEmailTree(emailTree);
 
 // writeToJsonFile(mapToObject(emailTree), "emailTree.json");
+// console.log(rawEmails.length, sortedEmailChains.flat().length);
 writeJsonToFile(sortedEmailChains, "output.json");
